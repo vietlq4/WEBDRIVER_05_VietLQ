b8ddcb56f3bc86201950f74d415dbc36c9b15fb8
vietleq <viet.leq@titancorpvn.com>
2018-07-15 12:24:26 +0700

    comit project
    Signed-off-by: vietleq <viet.leq@titancorpvn.com>

---
+++ src/test/java/Selenium_api/T01_CheckEnv.java
@@ -0,0 +1,44 @@
+package Selenium_api;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class T01_CheckEnv {
+   // System.set("webdriver.gecko.driver", "/Users/username/Downloads/geckodriver");
+    WebDriver driver ;
+
+    @Test
+    public void TC_01_CheckUrlAndTitle() {
+        System.out.println("Check homepage title");
+        String homePageTitle = driver.getTitle();
+        Assert.assertEquals(homePageTitle, "Guru99 Bank Home Page");
+
+        System.out.println("Check homepage url");
+        String homePageUrl = driver.getCurrentUrl();
+        Assert.assertEquals(homePageUrl, "http://demo.guru99.com/v4/");
+    }
+
+    @BeforeClass
+    public void beforeClass() {
+        System.setProperty("webdriver.gecko.driver", "/Users/mrtun/Documents/soft/geckodriver");
+        driver = new FirefoxDriver();
+        driver.get("http://demo.guru99.com/v4/");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+}
