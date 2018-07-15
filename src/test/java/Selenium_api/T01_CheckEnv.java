b143f3224ca2b6e4132d63c7f5c9b5db88b8b933
vietleq <viet.leq@titancorpvn.com>
2018-07-15 13:00:46 +0700

    comit project
    Signed-off-by: vietleq <viet.leq@titancorpvn.com>

--- src/test/java/Selenium_api/T01_CheckEnv.java
+++ src/test/java/Selenium_api/T01_CheckEnv.java
@@ -22,7 +22,7 @@ public class T01_CheckEnv {
         String homePageTitle = driver.getTitle();
         Assert.assertEquals(homePageTitle, "Guru99 Bank Home Page");
 
-        System.out.println("Check homepage urlmc");
+        System.out.println("Check homepage urc");
         String homePageUrl = driver.getCurrentUrl();
         Assert.assertEquals(homePageUrl, "http://demo.guru99.com/v4/");
     }
